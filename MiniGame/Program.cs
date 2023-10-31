d88e61778da256063bb9a3c9f96aa2efa8ea69b5
Adrian Dorey <144232716+adorey91@users.noreply.github.com>
2023-10-31 15:20:17 -0300

    Update Program.cs

--- MiniGame/Program.cs
+++ MiniGame/Program.cs
@@ -10,6 +10,48 @@ namespace MiniGame
     {
         static void Main(string[] args)
         {
+            Console.WriteLine("Mini Game");
+            Console.WriteLine();
+
+            //for loop
+            //--------
+            //// count 1 to 10
+            //for (int i = 1; i <= 10; i++) 
+            //{
+            //    Console.WriteLine(i);
+            //}
+
+            /* 3 parts:
+            1) declaration/initialization - start at 1 (outside of loop)
+            2) test -- at the top of loop
+            --- where the inside of the loop occures --- 
+            3) increment -- at bottom of loop
+            */
+
+            int x = 10; // player position
+            int y = 10; // player position
+            char avatar = '@';
+
+            bool gameOver = false;
+            // Game loop
+            while(gameOver != true)
+            {
+                ConsoleKeyInfo input;
+                input = Console.ReadKey();
+
+                if(input.Key == ConsoleKey.W)
+                {
+                    y = y - 1;
+                    if (y<0) y = 0; // creates border avatar won't go past
+                }
+
+                Console.SetCursorPosition(x, y); // player position
+                Console.WriteLine(avatar); // player avatar
+            }
+
+            Console.WriteLine();
+            Console.WriteLine("Press any key to exit..");
+            Console.ReadKey(true);
         }
     }
 }
